3ed4f932d5bce9a19382627d06315d8dafac9c85
Losciale-Verdille <45843664+Losciale-Verdille@users.noreply.github.com>
2019-04-15 23:59:13 +0200

    Update script.js

--- BatailleNavale2/script.js
+++ BatailleNavale2/script.js
@@ -24,7 +24,7 @@ let plateau=[];
 
 	
 	function genereTableau(){
-		let texte="<table class='table'>";
+		let texte="<p id=\"info\"><table class='table'>";
 		for (let i = 0; i <10; i++) {
 			texte+="<tr>";
 			for (let j = 0;j<10; j++) {
